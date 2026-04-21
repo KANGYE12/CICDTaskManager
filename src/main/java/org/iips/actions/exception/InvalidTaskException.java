cc92ff49bd4ec7701ac299982d1acb6199c0f866
kang Ye <kang2005123@gmail.com>
2026-04-21 12:41:01 +0200

    Introduce compile error

--- src/main/java/org/iips/actions/exception/InvalidTaskException.java
+++ src/main/java/org/iips/actions/exception/InvalidTaskException.java
@@ -7,7 +7,7 @@ public class InvalidTaskException extends RuntimeException {
    *
    * @param message Description of the validation error
    */
-  public InvalidTaskException(String message) {
+  public InvalidTaskException(Strings message) {
     super("Invalid task: " + message);
   }
 }
